a5658bf3366ab155077561fb2883548ba4d1deb7
Taylor Jackson <tjack94@hotmail.com>
2018-05-08 21:07:25 -0600

    classes

--- classes.js
+++ classes.js
@@ -30,8 +30,19 @@
 */
 
 //Code Here
+class Employee{
+  constructor(first_name,last_name,email,age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + " Widget"
+  }
 
-
+}
+var taylor = new Employee("taylor", "jackson", "tjack94@hotmail.com", 24)
 
 ////////// PROBLEM 2 //////////
 
@@ -50,9 +61,28 @@
 */
 
 //Code Here
+class Manager{
+  constructor(first_name,last_name,email,age,reports){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email
+    this.age = age
+    this.reports = []
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + " Widget"
+  }
+  hire(employee){
+    this.reports.push(employee)
 
+  }
+  fire(index){
+    this.reports.splice(index,1)
+  }
 
+}
 
+var doug = new Manager("doug", "smith","doug@gmail.com",45,["david","kevin"])
 ////////// PROBLEM 3 //////////
 
 /*
@@ -76,8 +106,75 @@
 */
 
 //Code Here
+class ProgressiveManager{
+  constructor(first_name,last_name,email,age,reports,title,bonus){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email
+    this.age = age
+    this.reports = []
+    this.title = "Not a manager"
+    this.bonus = 0
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + " Widget"
+  }
+  hire(employee){
+    this.reports.push(employee)
+    if(this.reports.length === 0){
+      this.title = "Not a manager"
+    
+    }else if(this.reports.length > 0 && this.reports.length < 4){
+      this.title = "Barely Manager"
+    }else if(this.reports.length > 3 && this.reports.length < 11){
+      this.title = "Mostly Manager"
+    }else if(this.reports.length > 10 && this.reports.length < 51){
+      this.title = "Manager"
+    }else if(this.reports.length > 50 && this.reports.length < 101){
+      this.title = "Manager Plus"
+    }else{
+      this.title = "Bestest Manager"
+    }
 
+  }
+  fire(index){
+    this.reports.splice(index,1);
+    this.bonus += 100
+    if(this.reports.length === 0){
+      this.title = "Not a manager"
+    
+    }else if(this.reports.length > 0 && this.reports.length < 4){
+      this.title = "Barely Manager"
+    }else if(this.reports.length > 3 && this.reports.length < 11){
+      this.title = "Mostly Manager"
+    }else if(this.reports.length > 10 && this.reports.length < 51){
+      this.title = "Manager"
+    }else if(this.reports.length > 50 && this.reports.length < 101){
+      this.title = "Manager Plus"
+    }else{
+      this.title = "Bestest Manager"
+    }
+    
+  }
+titleFinder(){
+  if(this.reports.length === 0){
+    this.title = "Not a manager"
+  
+  }else if(this.reports.length > 0 && this.reports.length < 4){
+    this.title = "Barely Manager"
+  }else if(this.reports.length > 3 && this.reports.length < 11){
+    this.title = "Mostly Manager"
+  }else if(this.reports.length > 10 && this.reports.length < 51){
+    this.title = "Manager"
+  }else if(this.reports.length > 50 && this.reports.length < 101){
+    this.title = "Manager Plus"
+  }else{
+    this.title = "Bestest Manager"
+  }
+}
 
+}
+var bob = new ProgressiveManager("bob", "Martin","bob@email.com", 38, ["kevin","carl","dave"])
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -103,5 +200,23 @@
 */
 
 //Code Here
-
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num
+    this.wear_and_tear_count += (num/50)
+  }
+  fixMachine(){
+    this.needs_reboot = true
+  }
+  reboot(){
+    this.wear_and_tear_count -= 10
+    this.reboot = false
+    return ()=> "I'm done robooting"
+  }
+}
 
